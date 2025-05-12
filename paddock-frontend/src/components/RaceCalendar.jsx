5f659ccca745caaced618c72ed109c5bca8f8676
Francisco Javier Cortez <francisco.cortez01@utrgv.edu>
2025-05-12 14:37:47 -0500

    Added round number next to grid box

--- paddock-frontend/src/components/RaceCalendar.jsx
+++ paddock-frontend/src/components/RaceCalendar.jsx
@@ -18,33 +18,54 @@ const raceData = [
     flag: "/assets/flags/USA.png",
     circuitMap: "/assets/circuits/miami-circuit.png",
   },
-  // ...add more races
+  // Add more races as needed
 ];
 
 const RaceCalendar = () => {
   return (
     <div className="calendarPage">
-      <div className="gridWrapper">
+      <div className="calendarContainer">
         {raceData.map((race, index) => (
-          <div
-            key={index}
-            className={`gridBox ${index % 2 === 0 ? "right" : "left"} ${
-              index > 0 ? "lower" : ""
-            }`}
-          >
-            <div className="gridBoxContent">
-              <div className="gridBoxTop">
-                <span className="roundLabel">ROUND {race.round}</span>
-                <img src={race.flag} alt={`${race.name} flag`} className="flagIcon" />
+          <div className="gridRow" key={index}>
+            {/* Left side number (if grid is right-aligned) */}
+            {index % 2 === 0 && (
+              <div className="roundNumberOutside">
+                <div className="roundText">Round</div>
+                <div className="roundDigit">{race.round}</div>
               </div>
-              <div className="dateRange">{race.dateRange}</div>
-              <div className="raceName">{race.name}</div>
-              <div className="raceTitle">{race.fullTitle}</div>
-              <img src={race.circuitMap} alt={`${race.name} circuit`} className="circuitMap" />
+            )}
+
+            {/* Grid Box */}
+            <div
+              className={`gridBox ${index % 2 === 0 ? "right" : "left"} ${
+                index > 0 ? "lower" : ""
+              }`}
+            >
+              <div className="gridBoxContent">
+                <div className="gridBoxTop">
+                  <img src={race.flag} alt={`${race.name} flag`} className="flagIcon" />
+                </div>
+                <div className="dateRange">{race.dateRange}</div>
+                <div className="raceName">{race.name}</div>
+                <div className="raceTitle">{race.fullTitle}</div>
+                <img
+                  src={race.circuitMap}
+                  alt={`${race.name} circuit`}
+                  className="circuitMap"
+                />
+              </div>
             </div>
+
+            {/* Right side number (if grid is left-aligned) */}
+            {index % 2 !== 0 && (
+              <div className="roundNumberOutside">
+                <div className="roundText">Round</div>
+                <div className="roundDigit">{race.round}</div>
+              </div>
+            )}
           </div>
         ))}
-      </div>
+        </div>
     </div>
   );
 };
